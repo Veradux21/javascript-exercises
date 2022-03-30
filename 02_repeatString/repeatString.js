25dd44ed6135c07284a60b38dc64bfac066bc5ec
Veradux21 <Veradux4@protonmail.com>
2022-03-30 15:36:20 -0400

    Complete Exercies 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let hostString = ""; 
+    if (num >= 0){
+        for (;num > 0; num--){
+            hostString += string;
+        }
+    } else {return 'ERROR'};
+    return hostString;
 };
 
 // Do not edit below this line
