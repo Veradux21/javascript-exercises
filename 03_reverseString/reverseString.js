b48b35babfb54ceb61ae9fba345e35b0d5906b35
Veradux21 <Veradux4@protonmail.com>
2022-04-03 17:10:35 -0400

    Complete 3, Stuck on 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let revStr = ""
+/*     for (let i = 0 ; i <= str.length; ++i){
+        revStr += str.slice(-i);
+    } */
+    let i = str.length - 1
+    while (i >= 0) { 
+        revStr += str[i]
+        i--;
+    }
+    return revStr;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+    let  acceptArray = new Array();
+    let i = array.length - 1;
+    console.log(array, array.length, remove, remove.length, acceptArray)
+    while (i >= 0){
+        let j = remove.length - 1;
+        console.log("i loop", "i:"+i, "j:"+j, array, array.length, remove, remove.length, 
+            acceptArray);
+        while (j >= 0){
+            if (array[i]==remove[j]){
+                array.pop();
+            }else{
+                acceptArray.push(array[i]);
+            }
+            console.log("j loop", "i:"+i, "j:"+j, array, array.length, remove, remove.length, 
+            acceptArray);
+            j--;
+        }
+        i--;
 
+    };
+    console.log("final Array: ", acceptArray);
+    return acceptArray.sort();
 };
 
 // Do not edit below this line
