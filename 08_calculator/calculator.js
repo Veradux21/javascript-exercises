95a21ae9e0c377dd2f0bf3c3db656e0c148639bc
Veradux21 <Veradux4@protonmail.com>
2022-06-14 15:33:53 -0400

    Complete calculator exercise
    created add, subtract, sum, multiply, power, factorial functions.
    Learned about array.reduce. Proved for loop, forEach arrow funct.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,50 @@
-const add = function() {
-	
+const add = function(...arg) {
+	let addResult = 0
+  addResult += arg[0]
+  addResult += arg[1]
+  return addResult
 };
 
-const subtract = function() {
-	
+const subtract = function(...arg) {
+  let tractResult = 0
+  tractResult += arg[0]
+  tractResult -= arg[1]
+  return tractResult
 };
 
-const sum = function() {
-	
+/* const sum = function(array) {
+  return array.reduce((total, current) => total + current, 0);
+}; */
+
+const sum = function(arg) {
+  return arg.reduce((total, args) => total + args, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  var total = 1
+  numbers.forEach(element => { 
+    total *= element;
+  });
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+	let total = num
+  for (let index = pow; index > 1; --index) {
+    total *= num;
+/*     console.log(total) */
+  }
+  return total
 };
 
-const factorial = function() {
-	
+const factorial = function(target) {
+	let total = 1
+  if(target == 0){return 1}
+  for (let index = 1; index <= target ; index++) {
+    total *= index;
+    console.log(" "+target, " "+index, " "+total)
+  }
+  return total;
 };
 
 // Do not edit below this line
