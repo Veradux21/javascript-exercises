d8f6bda67f552f531dc332ae3e9524aa24438223
Veradux21 <Veradux4@protonmail.com>
2022-06-13 23:01:11 -0400

    Complete exercise 05, sumAll
    Used multiple if statements to validate for erroneous arguments
    Used if statement to validate that the smaller value was assigned to first
    For loop to add each number to sum and increment counter by 1 until it equals larger value

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(...args) {
+    let first = args[0]
+    let second = args[1]
+    let sum = 0
+    if (!Number.isInteger(first)||!Number.isInteger(second)){return 'ERROR';};
+    if (first < 0 || second < 0){return 'ERROR';};
+    if (first>second){
+        let swap = first;
+        first = second;
+        second = swap;
+    };
+    for (let i = first; i <= second; i++){
+        sum += i
+        console.log(sum)
+    }
+    return sum;
 };
 
 // Do not edit below this line
