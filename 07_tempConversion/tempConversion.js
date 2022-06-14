a6e74bc4ba57a15b0277474fa87b6298c1a1c1fa
Veradux21 <Veradux4@protonmail.com>
2022-06-14 13:31:01 -0400

    Complete exercise tempConversion
    Learned about the benefits of using Math.round and limits of toPrecision and toFixed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,25 @@
-const ftoc = function() {
-
-};
-
-const ctof = function() {
-
+const ftoc = function(temp) { 
+  temp -= 32;
+  console.log("+32:"+temp)
+  temp *= 5;
+  console.log("*5: "+temp)
+  temp /= 9;
+  console.log("/9: "+temp)
+  temp = temp.toPrecision(3);
+  //toPrecision is a problem when output is not two digit whole number. Should use Math.round(num * 10)/10
+  console.log("precision: "+temp)
+  temp = parseFloat(temp)
+  return temp
+}; 
+  
+ const ctof = function(temp) {
+  temp *= 9;
+  temp /= 5;
+  temp += 32;
+  temp = Math.round(temp * 10) / 10
+/*   temp = temp.toString;
+  temp = parseFloat(temp) */
+  return temp 
 };
 
 // Do not edit below this line
