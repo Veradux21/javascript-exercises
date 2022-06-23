ab61f4506796568275c1eb184c7fd8e55e93e733
Veradux21 <Veradux4@protonmail.com>
2022-06-23 17:45:46 -0400

    Complete palindromes exercise
    Utilized reverse string function and other string methods, including replace regex, to compare phrase backwards and forwards.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+const reverseString = function(str) {
+    let revStr = ""
+/*     for (let i = 0 ; i <= str.length; ++i){
+        revStr += str.slice(-i);
+    } */
+    let i = str.length - 1
+    while (i >= 0) { 
+        revStr += str[i]
+        i--;
+    }
+    return revStr;
+};
 
+const palindromes = function (phrase) {
+    console.log(phrase)
+    phrase = phrase.toLowerCase();
+    console.log(phrase);
+    phrase = phrase.replace( /(\W+)/g , "");
+    console.log(phrase)
+    let revPhrase = reverseString(phrase);
+    console.log(revPhrase);
+    if (phrase === revPhrase){return true} else {return false};
 };
 
 // Do not edit below this line
