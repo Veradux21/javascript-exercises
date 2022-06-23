708b4310ec2710a9e30b90f46bd4d0337d5b83b7
Veradux21 <Veradux4@protonmail.com>
2022-06-23 18:05:18 -0400

    Complete fibonacci exercisee

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(nbr) {
+    nbr = Number(nbr);
+    if (nbr<1){return "OOPS"};
+    let fib1 = 0;
+    let fib2 = 1;
+    let fib = nbr;
+    for(let i = 2; nbr >= i; i++ ){
+        fib = fib1 + fib2;
+        fib1 = fib2;
+        fib2 = fib;
+    };
+    return fib;
 
 };
 
